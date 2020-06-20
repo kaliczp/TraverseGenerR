a728a8ad746e0fa74425b68523d9a266d08edf23
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-06-20 13:17:24 +0200

    More informative plot
    Plot with labels and point names.

--- process.R
+++ process.R
@@ -3,7 +3,10 @@ coo[7, 1] <- "SP"
 coo[,"X37"] <- as.numeric(as.character(coo[,"X37"]))
 coo[,"X38"] <- as.numeric(as.character(coo[,"X38"]))
 coo[,"X39"] <- as.numeric(as.character(coo[,"X39"]))
-plot(X37 ~ X38, coo)
+
+plot(X37 ~ X38, coo, asp=T)
+text(coo$X38, coo$X37, lab=coo$X5, adj=c(1,0))
+text(coo$X38, coo$X37, lab=coo$X4, adj=c(1,1))
 
 gizi.read("teszt.geo")
 gizi.read("teszt.geo", 2)
