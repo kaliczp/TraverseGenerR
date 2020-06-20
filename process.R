8ca603bcc5f60e0d7f1e89236026b8e89a1fa4c8
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-06-20 21:34:40 +0200

    Refine graph, save import into object and merge traverse
    Some grafical enhancement with traverse line.
    
    Imported objects saved into permanet object and traverse segments
    merged together.

--- process.R
+++ process.R
@@ -7,14 +7,24 @@ coo[,"X39"] <- as.numeric(as.character(coo[,"X39"]))
 
 ## Plot coordinates
 plot(X37 ~ X38, coo, asp=T)
+lines(X37 ~ X38, coo[c(nrow(coo),1:(nrow(coo)-1)),])
 text(coo$X38, coo$X37, lab=coo$X5, adj=c(1,0))
 text(coo$X38, coo$X37, lab=coo$X4, adj=c(1,1))
 
+
+text(coo$X38, coo$X37, lab=1:nrow(coo), adj=c(1,0))
+
 ## Read in measurements
 ##   Station data
-gizi.read("teszt.geo")
+Stat <- gizi.read("teszt.geo")
 ##   Initial measurement
-gizi.read("teszt.geo", 2)
+ttinit <- gizi.read("teszt.geo", 2)
 ##   Traverse
-gizi.read("teszt.geo", 4)
+Traverse <- gizi.read("teszt.geo", 4)
 
+## Merge Traverse measurements
+for(tti in 3:ncol(Traverse))
+    Traverse[,tti] <- as.numeric(Traverse[,tti])
+Traverse[2,1:2] <- na.omit(ttinit)[,1:2]
+Traverse[2,3:ncol(ttinit)] <- as.numeric(na.omit(ttinit)[,3:ncol(ttinit)])
+rm(ttinit)
