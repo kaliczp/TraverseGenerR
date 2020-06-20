ef185ca991a8f7f4857c18e1b5f330d4eab8b748
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-06-20 13:34:09 +0200

    Comments added

--- process.R
+++ process.R
@@ -1,13 +1,20 @@
+## Read in coordinates
 coo <- gizi.read("teszt.coo")
 coo[7, 1] <- "SP"
 coo[,"X37"] <- as.numeric(as.character(coo[,"X37"]))
 coo[,"X38"] <- as.numeric(as.character(coo[,"X38"]))
 coo[,"X39"] <- as.numeric(as.character(coo[,"X39"]))
 
+## Plot coordinates
 plot(X37 ~ X38, coo, asp=T)
 text(coo$X38, coo$X37, lab=coo$X5, adj=c(1,0))
 text(coo$X38, coo$X37, lab=coo$X4, adj=c(1,1))
 
+## Read in measurements
+##   Station data
 gizi.read("teszt.geo")
+##   Initial measurement
 gizi.read("teszt.geo", 2)
+##   Traverse
 gizi.read("teszt.geo", 4)
+
