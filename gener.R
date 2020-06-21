225fcf5e2fd29b24b69cd07847dc6c1df09683fd
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-06-21 06:24:42 +0200

    More segments and z coordinates added

--- gener.R
+++ gener.R
@@ -1,14 +1,23 @@
 newcoo <- coo[10:9,]
 
-gener <- function(ox=464800,oy=259400){
+gener <- function(slope=6, ox=464800,oy=259400, oz=250){
     fulldist <- sample(400:500,1)
     angledist <- sample(50:100,1)
-    frame <- data.frame(x=c(angledist,0,0),
-                        y=c(fulldist,fulldist,0))
+    segments.nr <- sample(5:7, 1)
+    segment.length <- fulldist/segments.nr
+    frame <- data.frame(x=c(angledist,rep(0,segments.nr+1)),
+                        y=c(fulldist,
+                            seq(fulldist, 0, -segment.length))
+                        )
+    frame$x <- frame$x + rnorm(nrow(frame), sd = 20)
+    frame$y <- frame$y + rnorm(nrow(frame), sd = 5)
+    frame$z <- -slope/100*frame$y 
     ## Translate
     frame$x <- frame$x + ox
     frame$y <- frame$y + oy
+    frame$z <- frame$z + oz
     frame
 }
 
-plot(gener(), asp=TRUE)
+tteszt <- gener()
+plot(tteszt, asp=TRUE)
