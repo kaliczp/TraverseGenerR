140e77eac2d048f108a2b99c7b8a1edf92ffed59
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-07-04 12:38:10 +0200

    Two poligons
    Second poligon is attributed. Similart to the first.

--- datgen.R
+++ datgen.R
@@ -55,8 +55,11 @@ datgen <- function(x, file, settlement = "Sehonna", objkod = 105201, student = "
                       sep = "*")
                 )
     WriteDATRow("T_OBJ_ATTRBD")
-    HRSZ1 <- paste0("0", sample(10:100, 1))
+    hrsz1 <- sample(10:100, 1)
+    HRSZ1 <- paste0("0", hrsz1)
+    HRSZ2 <- paste0("0", hrsz1 + sample(1:5, 1))
     area1 <- 1245
+    area2  <- 1150
     mod.Dat <- act.date - sample(900:4100, 1)
     mod.date  <- format(mod.Dat, "%Y%m%d")
     law.code <- paste(sample(2:20,1),sample(300:500,1),format(mod.Dat, "%Y"), sep="/")
@@ -74,6 +77,20 @@ datgen <- function(x, file, settlement = "Sehonna", objkod = 105201, student = "
                       "", # codepoint
                       sep = "*")
                 )
+    WriteDATRow(paste(2,
+                      "BD02",
+                      2, # area id
+                      HRSZ2,
+                      "",
+                      2, # location rural
+                      area2,
+                      "********2*1*487",
+                      mod.date,
+                      law.code, # Changing law
+                      "***0*114*",
+                      "", # codepoint
+                      sep = "*")
+                )
     WriteDATRow("T_OBJ_ATTRBE")
     WriteDATRow(paste(1,
                       "BE03",
@@ -81,6 +98,22 @@ datgen <- function(x, file, settlement = "Sehonna", objkod = 105201, student = "
                       "-",
                       HRSZ1,
                       area1,
+                      "", # value
+                      8, # Forest
+                      2, # location rural
+                      1,
+                      1, # prev. valid rec.
+                      mod.date,
+                      "***0*120*",
+                      "", # codepoint
+                      sep = "*")
+                )
+    WriteDATRow(paste(2,
+                      "BE03",
+                      2, # area id
+                      "-",
+                      HRSZ2,
+                      area2,
                       "", # value
                       8, # Forest
                       2, # location rural
@@ -108,6 +141,22 @@ datgen <- function(x, file, settlement = "Sehonna", objkod = 105201, student = "
                       area1,
                       sep = "*")
                 )
+    WriteDATRow(paste(1,
+                      "BF01",
+                      2, # area id
+                      5, # quality code
+                      8, # forest
+                      1, # rural
+                      1,
+                      1, # last valid
+                      0,
+                      118,
+                      "",
+                      "",
+                      HRSZ2,
+                      area2,
+                      sep = "*")
+                )
     WriteDATRow("T_FELIRAT")
     WriteDATRow(paste(1,
                       HRSZ1,
@@ -128,6 +177,28 @@ datgen <- function(x, file, settlement = "Sehonna", objkod = 105201, student = "
                       0, # valid date
                       "T_OBJ_ATTRBD",
                       1, # row id
+                      14, # property HRSZ
+                      sep = "*")
+                )
+    WriteDATRow(paste(3,
+                      HRSZ2,
+                      5, # lower left corner point id
+                      90, # orientation
+                      6, # font size
+                      0, # valid date
+                      "T_OBJ_ATTRBD",
+                      2, # row id
+                      11, # property HRSZ
+                      sep = "*")
+                )
+    WriteDATRow(paste(4,
+                      "E",
+                      5, # lower left corner point id
+                      90, # orientation
+                      2, # font size
+                      0, # valid date
+                      "T_OBJ_ATTRBD",
+                      2, # row id
                       14, # property HRSZ
                       sep = "*")
                 )
