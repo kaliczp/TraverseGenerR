f4111c70f1664bddb04f87c9674e9baf82049277
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-07-10 21:37:20 +0200

    Unit field treated separately in m5 file generation
    Units now separated field with default "m". First row modified
    accordingly the units default.

--- m5export.R
+++ m5export.R
@@ -19,16 +19,28 @@ export.m5 <- function(projectname = "Default", angle = NULL, coordinates = NULL)
                          field1 = "",
                          code2 = "",
                          field2 = "",
+                         unit2 = "m",
                          code3 = "",
                          field3 = "",
+                         unit3 = "m",
                          code4 = "",
                          field4 = "",
+                         unit4 = "m",
                          closing = " ") {
         paste(align.field(fieldtext = row.num, width = 5, alignment = "right"),
-              align.field(paste(code1, field1), width = 30),
-              align.field(paste(code2, field2), width = 22),
-              align.field(paste(code3, field3), width = 22),
-              align.field(paste(code4, field4), width = 22),
+              align.field(paste(code1, field1), width = 31),
+              paste(align.field(code2, width = 2),
+                    align.field(field2, width = 14, alignment = "right"),
+                    align.field(unit2, width = 4)
+                    ),
+              paste(align.field(code3, width = 2),
+                    align.field(field3, width = 14, alignment = "right"),
+                    align.field(unit3, width = 4)
+                    ),
+              paste(align.field(code4, width = 2),
+                    align.field(field4, width = 14, alignment = "right"),
+                    align.field(unit4, width = 4)
+                    ),
               closing,
               sep = "|"
               )
@@ -38,8 +50,8 @@ export.m5 <- function(projectname = "Default", angle = NULL, coordinates = NULL)
     ## Create header
     row.num <- 1
     result <- make.row(field1 = paste("Project",
-                                      align.field(projectname, width = 19)
-                                      )
+                                      align.field(projectname, width = 20)
+                                      ), unit2 = "", unit3 = "", unit4 = ""
                        )
     row.num <- 2
     paste("For M5|Adr", result)
