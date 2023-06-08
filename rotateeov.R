a48e3f0b1c63aa60d78e680f2297a7ecb11d6f16
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2023-06-08 18:43:48 +0200

    Better indexing in rotateeov in the case of explicit cordinates

--- rotateeov.R
+++ rotateeov.R
@@ -1,13 +1,18 @@
 ## https://geocompr.robinlovelace.net/geometric-operations.html#affine-transformations
 ## More simpler solution
-eovrotate <- function(x, degree = 10) {
+eovrotate <- function(x, degree = 10, shiftcoo = NULL) {
     radian.angle <- degree * (pi / 180)
     ## Calculate means
     col.means <- colMeans(x[,c("x","y")])
     ## Translate to origin
     shifted.coo <- x[,c("x","y")]
+    if(is.null(shiftcoo)) {
     shifted.coo$x <- shifted.coo$x - col.means["x"]
     shifted.coo$y <- shifted.coo$y - col.means["y"]
+    } else {
+        shifted.coo$x <- shifted.coo$x - shiftcoo["x"]
+        shifted.coo$y <- shifted.coo$y - shiftcoo["y"]
+    }
     temporary <- shifted.coo
     shifted.coo$x <- (temporary$x * cos(radian.angle)) -
         (temporary$y * sin(radian.angle))
